97d936db467892e9253618bb9316d1f33f893410
Mohit Bhargava <151164061+Mohitb1202@users.noreply.github.com>
2025-05-19 19:34:49 +0530

    Update main.py

--- demo/main.py
+++ demo/main.py
@@ -4,21 +4,7 @@ from .database import engine
 from .routers import users,login,fileupload
 
 
-app=FastAPI(
-    title="Products API",
-    description="Get details for all the products on our website",
-    terms_of_service="http://www.google.com",
-    contact={
-        "Developer Name":"Demo patel",
-        "website":"http://www.google.com",
-        "email":"demo@gmail.com"
-    },
-    license_info={
-        "name":"XYZ",
-        "url":"http://www.google.com"
-    },
-    #docs_url="/documentation",redoc_url=None
-)
+app=FastAPI()
 
 
 
