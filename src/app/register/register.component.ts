d100ee01a92df13c29036eea7968a677434dd7b3
Mohit Bhargava <151164061+Mohitb1202@users.noreply.github.com>
2025-05-28 17:39:16 +0530

    Update register.component.ts

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
-import { UserService } from '../Polyclinic-services/user-service/user.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -21,16 +21,6 @@ export class RegisterComponent implements OnInit{
       password: [""],
       gender:[""]
     })
-  }
-
-  submitForm(form: FormGroup) {
-    this._userService.register(form.value.userName, form.value.emailId, form.value.contactNumber, form.value.password, form.value.gender).subscribe(
-      responseRegisterData => {
-        this.status = responseRegisterData;
-        console.log(this.status);
-        alert("Registration successful")
-      }
-    )
   }
 
 }
